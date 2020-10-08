0467240760d02144dc140cd7f7b549544e0c641d
Strnad Matěj <strnad@spstrutnov.cz>
2020-10-08 14:50:51 +0200

    opraveno vyhození chyby, pokud se někdo nepřihlášený pokusí dostat do příspěvků

--- src/views/prispevky/nezobrazit.php
+++ src/views/prispevky/nezobrazit.php
@@ -1,1 +1,11 @@
+<?php
+    global $zakladni_url;
+
+    if(!isset($_SESSION["prihlaseny_uzivatel"]))
+    {
+        header("location:".$zakladni_url."index.php/stranky/chyba/");
+        die();
+    }
+?>
+
 <p>Nemáte zatím žádné příspěvky.</p>
